d2d9643049085bc443d33d61be4823d1092a5a7e
sdana <seth.dana@gmail.com>
2018-07-17 15:36:57 -0500

    Added test button to gen random number to change state

--- src/Components/Friends/Friends.js
+++ src/Components/Friends/Friends.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
 
 export default class Friends extends Component {
+    state = {
+        number: 1
+    }
+    genRando=()=>{
+        let newNum = Math.floor(Math.random()*10+1)
+        this.setState({number: newNum})
+    }
     render(){
         return (
             <React.Fragment>
                 <div className="friends">
                     <h4>Friends</h4>
+                    <h3>{this.state.number}</h3>
+                    <button onClick={this.genRando}>New Number</button>
                 </div>
             </React.Fragment>
         )
