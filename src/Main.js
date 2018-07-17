2d5ba3fa0debaf6035c6fe05cec4417a7657632c
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-07-17 15:01:07 -0500

    Added grid template file

--- src/Main.js
+++ src/Main.js
@@ -2,21 +2,20 @@ import React, { Component } from 'react'
 import News from './Components/News/News';
 import Tasks from "./Components/Tasks/Tasks"
 import Chat from "./Components/Chat/Chat"
-import EventList from './Components/Events/EventList';
+import Events from './Components/Events/EventList';
 import Friends from "./Components/Friends/Friends"
 
 
 export default class Main extends Component {
     render() {
         return (
-                      <React.Fragment>
-
-            < News />
-            <Friends />
-            <Chat />
-            <Events />
-           <Tasks />
-                      </React.Fragment>
+            <React.Fragment>
+                <News />
+                <Friends />
+                <Chat />
+                <Events />
+                <Tasks />
+            </React.Fragment>
 
 
         )
