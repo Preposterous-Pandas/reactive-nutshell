37916e3eb713f94bc8f755558caa2adc7ea77eaa
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-07-18 15:53:59 -0500

    Got Moment.js working to parse duedate fields

--- src/Components/Tasks/TaskCard.js
+++ src/Components/Tasks/TaskCard.js
@@ -1,10 +1,15 @@
 import React from "react"
+import Moment from "react-moment"
 
 export default props => {
   return (
     <section className="task__card">
-      <h4>{props.currentTask.description}</h4>
-      <p>Due: {props.currentTask.date}</p>
+      <h3>{props.currentTask.description}</h3>
+      <p>Due: <Moment format="ddd, MMM Do, YYYY">
+        {props.currentTask.date}
+      </Moment></p>
+      <label>Completed?</label>
+      <input type="checkbox" />
     </section>
   )
 }
