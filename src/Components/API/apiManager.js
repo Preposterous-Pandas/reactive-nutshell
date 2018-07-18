a0b23869825644d18e2849d15b47ac50c9e5d17a
Philip Patton <ppatton35745@gmail.com>
2018-07-18 11:00:44 -0500

    before master merge

--- src/Components/API/apiManager.js
+++ src/Components/API/apiManager.js
@@ -5,119 +5,98 @@ export default class apiManager {
     return fetch(`http://localhost:5002/${resource}`).then(e => e.json());
   }
 
-  // allFriends() {
-  //   return fetch(`http://localhost:5002/friends`)
-  //     .then(e => e.json())
-  //     .then(friends => {
-  //       const fList = [];
-  //       const User = sessionStorage.getItem("User");
-  //       friends.forEach(friend => {
-  //         if (friend.yourId == User) {
-  //           fList.push(friend.userId);
-  //         }
-  //       });
-  //       return fList;
-  //     });
-  // }
+  getField(resource) {
+    return fetch(`http://localhost:5002/${resource}`).then(e => e.json());
+  }
 
-  getFriendsList() {
-    return fetch(`http://localhost:5002/friends`).then(e => e.json());
-}
+  getFriendsList(currentUserId) {
+    return fetch(
+      `http://localhost:5002/friends?_expand=user&yourId=${currentUserId}`
+    ).then(e => e.json());
+  }
 
-addNewFriend(currentUserId, friendToAddId) {
-        return fetch("https://localhost:5002/friends", {
-            
-class apiManager {
+  addNewFriend(currentUserId, friendToAddId) {
+    return fetch("https://localhost:5002/friends", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        userId: friendToAddId,
+        yourId: currentUserId
+      })
+    });
+  }
 
-   getField(resource) {
-        return fetch(`http://localhost:5002/${resource}`).then(e => e.json())
-    }
+  deleteFriend(relId) {
+    return fetch(`http://localhost:5002/friends/${relId}`, {
+      method: "DELETE"
+    });
+  }
 
-    allFriends() {
-        return fetch(`http://localhost:5002/friends`)
-            .then(e => e.json())
-            .then(friends => {
-                const fList = [];
-                const User = sessionStorage.getItem("User");
-                friends.forEach(friend => {
-                    if (friend.yourId == User) {
-                        fList.push(friend.userId);
-                    }
-                });
-                return fList;
-            })
-    }
+  postUser(name, email) {
+    return fetch("http://localhost:5002/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email
+      })
+    });
+  }
 
-    postUser(name, email) {
-        return fetch("http://localhost:5002/users",
-            {
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    "name": name,
-                    "email": email
-                })
-            })
-    }
+  getUser(userId) {
+    return fetch(`http://localhost:5002/users/${userId}`).then(e => e.json());
+  }
 
-    getUser(userId) {
-        return fetch(`http://localhost:5002/users/${userId}`).then(e => e.json())
-    }
+  getMessage(messageId) {
+    return fetch(`http://localhost:5002/messages/${messageId}`).then(e =>
+      e.json()
+    );
+  }
 
-    getMessage(messageId) {
-        return fetch(`http://localhost:5002/messages/${messageId}`).then(e => e.json())
-    }
+  postMessage(msg, user) {
+    return fetch("http://localhost:5002/messages", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        userId: user,
+        message: msg
+      })
+    });
+  }
 
-    postMessage(msg, user) {
-        return fetch("http://localhost:5002/messages", {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                "userId": user,
-                "message": msg
-            })
-        })
-    }
+  putMessage(user, msg, id) {
+    return fetch(`http://localhost:5002/messages/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        userId: user,
+        message: msg
+      })
+    });
+  }
 
-    putMessage(user, msg, id) {
-        return fetch(`http://localhost:5002/messages/${id}`, {
-            method: "PUT",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                "userId": user,
-                "message": msg
-            })
-        })
-    }
-
-    postEvent(user, name, loc, date) {
-        return fetch("http://localhost:5002/events", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                userId: friendToAddId,
-                yourId: currentUserId
-            })
-        });
-    }
-}
-
-
-
-deleteFriend(relId){
-  return fetch(`http://localhost:5002/friends/${relId}`, {
-    method: "DELETE"
-  });
-}
-
+  postEvent(user, name, loc, date) {
+    return fetch("http://localhost:5002/events", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        userId: user,
+        name: name,
+        location: loc,
+        date: date
+      })
+    });
+  }
 
   postUser(name, password, email) {
     return fetch("http://localhost:5002/users", {
@@ -137,17 +116,11 @@ deleteFriend(relId){
     return fetch(`http://localhost:5002/users/${userId}`).then(e => e.json());
   }
 
-  // getMessages() {
-  //   return fetch(
-  //     "https://localhost:5002/messages?_expand=user&_sort=timeStamp"
-  //   ).then(e => e.json());
-  // }
-
-  // //   getMessage(messageId) {
-  // //     return fetch(`http://localhost:5002/messages/${messageId}`).then(e =>
-  // //       e.json()
-  // //     );
-  // //   }
+  getMessages() {
+    return fetch(
+      "https://localhost:5002/messages?_expand=user&_sort=timeStamp"
+    ).then(e => e.json());
+  }
 
   postMessage(userId, message, timestamp) {
     return fetch("http://localhost:5002/messages", {
@@ -163,19 +136,6 @@ deleteFriend(relId){
     });
   }
 
-  // //   postMessage(msg, user) {
-  // //     return fetch("http://localhost:5002/messages", {
-  // //       method: "POST",
-  // //       headers: {
-  // //         "Content-Type": "application/json"
-  // //       },
-  // //       body: JSON.stringify({
-  // //         userId: user,
-  // //         message: msg
-  // //       })
-  // //     });
-  // //   }
-
   putMessage(msgId, userId, newMessage, messageTimeStamp) {
     return fetch(`http://localhost:5002/messages/${msgId}`, {
       method: "PUT",
@@ -189,19 +149,6 @@ deleteFriend(relId){
       })
     });
   }
-
-  // //   putMessage(user, msg, id) {
-  // //     return fetch(`http://localhost:5002/messages/${id}`, {
-  // //       method: "PUT",
-  // //       headers: {
-  // //         "Content-Type": "application/json"
-  // //       },
-  // //       body: JSON.stringify({
-  // //         userId: user,
-  // //         message: msg
-  // //       })
-  // //     });
-  // //   }
 
   delMessage(msgId) {
     return fetch(`http://localhost:5002/messages/${msgId}`, {
@@ -296,27 +243,4 @@ deleteFriend(relId){
       })
     });
   }
-
-  // postFriend(user, yourid) {
-  //   return fetch("http://localhost:5002/friends", {
-  //     headers: {
-  //       "Content-Type": "application/json"
-  //     },
-  //     method: "POST",
-  //     body: JSON.stringify({
-  //       userId: user,
-  //       yourId: yourid
-  //     })
-  //   });
-  // }
-
-  // delFriend(id) {
-  //   return fetch(`http://localhost:5002/friends/${id}`, {
-  //     method: "DELETE"
-  //   });
-  // }
 }
-
-const API = new apiManager;
-export default API;
-
