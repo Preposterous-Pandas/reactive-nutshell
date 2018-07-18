20640a2bc370fc4b6311ebf24c7a58e55bf2470e
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-07-18 11:49:22 -0500

    Added some comments to NewTaskSection

--- src/Components/Tasks/NewTaskSection.js
+++ src/Components/Tasks/NewTaskSection.js
@@ -6,7 +6,11 @@ import apiManager from "../API/apiManager"
 export default class NewTaskSection extends Component {
 
   state = {
+    // This property is used in the render function is to determine whether to show the Add Task button or the New Task form
+    // It is toggled to 'true' by clicking the Add Task button and to 'false' by clicking the Save Task button on the form
     addingTask: false,
+    
+    // These two properties are updated by handleFieldChange() and used in addNewTask() as the properties that get passed to the POST
     newTaskName: "",
     newTaskDate: ""
   }
@@ -36,6 +40,7 @@ export default class NewTaskSection extends Component {
 
   render() {
 
+    // This conditional checks if you are adding a new task. If so, it prints the form to add the task; if not, it prints the button which, when clicked, will open the form by changing state
     if (this.state.addingTask) {
       return (
         <form onSubmit={this.addNewTask}>
