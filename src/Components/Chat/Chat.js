4e9a8b3f6c59f25bfa4bd235afb5f329c5f7365b
sdana <seth.dana@gmail.com>
2018-07-18 10:17:10 -0500

    new chat file

--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -1,52 +1,104 @@
-import React, { Component } from "react";
-// import Animal from "./Animal";
-// import APIManager from "./APIManager";
+import React, { Component } from "react"
+import APIManager from "../API/apiManager"
+import "./chat.css"
+import Message from "./Message"
+
+const messagesApi = new APIManager()
 
 export default class Chat extends Component {
-  state = {};
+  state = {
+    currentUser: "",
+    buildSource: "",
+    newMessageInput: "",
+    messages: [],
+    editMsgButtonDisplay: false
+  }
+
+  read = buildSource => {
+    messagesApi.getMessages().then(msgArr => {
+      this.setState({
+        messages: msgArr
+      })
+      this.setState({
+        buildSource: buildSource
+      })
+    })
+  }
+
+  create = (userId, message) => {
+    const curTimeStamp = new Date()
+    messagesApi.postMessage(userId, message, curTimeStamp).then(() => {
+      this.read("createNew")
+    })
+  }
+
+  update = (msgId, userId, newMessage, messageTimeStamp) => {
+    messagesApi
+      .putMessage(msgId, userId, newMessage, messageTimeStamp)
+      .then(() => {
+        this.read()
+      })
+  }
+
+  delete = msgId => {
+    messagesApi.delMessage(msgId).then(this.read)
+  }
+
+  isAuthenticated = () => {
+    return (
+      sessionStorage.getItem("credentials") ||
+      localStorage.getItem("credentials")
+    )
+  }
 
   componentDidMount() {
-    // APIManager.getAllAnimals().then(animals =>
-    //   this.setState({
-    //     animals: animals
-    //   })
-    // );
+    const currentUser = this.isAuthenticated()
+    if (currentUser) {
+      this.setState({
+        currentUser: currentUser
+      })
+      this.read()
+    }
   }
 
   handleFieldChange = evt => {
-    // const stateToChange = {};
-    // // stateToChange.animals = this.state.animals;
-    // stateToChange[evt.target.id] = evt.target.value;
-    // this.setState(stateToChange);
-  };
-
-  createNewAnimal = (animalName, animalBreed) => {
-    // APIManager.createNewAnimal(animalName, animalBreed).then(() => {
-    //   APIManager.getAllAnimals().then(animalList => {
-    //     this.setState({
-    //       animals: animalList
-    //     });
-    //   });
-    // });
-  };
-
-  checkOutAnimal = animalId => {
-    // APIManager.checkOutAnimal(animalId).then(() => {
-    //   APIManager.getAllAnimals().then(animalList => {
-    //     this.setState({
-    //       animals: animalList
-    //     });
-    //   });
-    // });
-  };
+    const stateToChange = {}
+    stateToChange[evt.target.id] = evt.target.value
+    this.setState(stateToChange)
+  }
 
   render() {
     return (
-      <React.Fragment>
-        <div className="chat">
-          <h2>Chat</h2>
+      <div className="chat" id="messagesDiv">
+        <div id="messengerHeaderDiv">
+          <h2 className="messengerHeader" />
+          <button id="msgOptionButton">
+            <i className="fa fa-cogs" />
+          </button>
         </div>
-      </React.Fragment>
-    );
+        <div id="messengerBodyDiv">
+          <div id="messengerBodyContent">
+            {this.state.messages.map(message => (
+              <Message
+                key={message.id}
+                message={message}
+                create={this.create}
+                read={this.read}
+                update={this.update}
+                delete={this.delete}
+                currentUser={this.currentUser}
+                editMsgButtonDisplay={this.editMsgButtonDisplay}
+              />
+            ))}
+          </div>
+        </div>
+        <div id="messageNewDiv">
+          <input id="newMessageInput" />
+          <button id="newMessageSubmitButton">
+            <i className="fa fa-paper-plane-o" />
+          </button>
+        </div>
+      </div>
+    )
   }
 }
