c74519556fc8c5b9026e4d8881f43bbff0fcb8c8
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-07-19 10:30:46 -0500

    Have an edit link that toggles the input form and the task card, but does not save yet

--- src/Components/Tasks/TaskCard.js
+++ src/Components/Tasks/TaskCard.js
@@ -4,29 +4,65 @@ import apiManager from "../API/apiManager"
 
 export default class TaskCard extends Component {
 
+  state = {
+    editingTask: false,
+    taskName: ""
+  }
+
+  // This function gets the id of the box clicked, parses it into an integer, passes the integer into the completeTask method from the apiManager, and then loads all the tasks
   completeTask = (evt) => {
-    console.log("Box checked:");
-    const completionStatus = (evt.target.checked);
-    console.log(completionStatus);
     const taskId = parseInt(evt.target.id);
-    console.log(taskId);
     apiManager.completeTask(taskId)
       .then(this.props.loadTasks);
+  }
+
+  // This function gets the id of the edit
+  editTask = (evt) => {
+    evt.preventDefault();
+    console.log("Edit clicked:");
+    this.setState({ editingTask: true });
+    const editId = parseInt(evt.target.id);
+    console.log(editId);
 
   }
 
+  saveTask = (evt) => {
+    evt.preventDefault();
+    console.log("Saving...");
+    this.setState({ editingTask: false });
+  }
+
   render() {
-    return (
-      <section className="task__card">
-        <h3>{this.props.currentTask.description}</h3>
-        <p>Due: <Moment format="ddd, MMM Do, YYYY">
-          {this.props.currentTask.date}
-        </Moment></p>
-        <label>Completed?</label>
-        <input type="checkbox"
-          id={`${this.props.currentTask.id}checkbox`}
-          onChange={this.completeTask} />
-      </section>
-    )
+    if (this.state.editingTask) {
+      return (
+        <form onSubmit={this.saveTask}>
+          <label>New task description:</label>
+          <input type="text"
+            id={`${this.props.currentTask.id}input`}
+            defaultValue={this.props.currentTask.description} />
+            <button type="submit">Save changes</button>
+        </form>
+      )
+    } else {
+      return (
+        <section className="task__card">
+          <h3>{this.props.currentTask.description}</h3>
+
+          <a href=""
+            id={`${this.props.currentTask.id}edit`}
+            onClick={this.editTask}>(edit task description)</a>
+
+          <p>Due: <Moment format="ddd, MMM Do, YYYY">
+            {this.props.currentTask.date}
+          </Moment></p>
+
+          <label>Completed?</label>
+          <input type="checkbox"
+            id={`${this.props.currentTask.id}checkbox`}
+            onChange={this.completeTask} />
+
+        </section>
+      )
+    }
   }
 }
