980c69a9af33c2f02e77ef045a1c953244f3918d
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-07-20 16:00:25 -0500

    Heavy refactor: form now loads on both sections and is agnostic

--- src/Components/Tasks/TaskCard.js
+++ src/Components/Tasks/TaskCard.js
@@ -1,15 +1,13 @@
 import React, { Component } from "react"
 import Moment from "react-moment"
 import apiManager from "../API/apiManager"
+import TaskForm from "./TaskForm"
 
 
 export default class TaskCard extends Component {
 
   state = {
     editingTask: false,
-    taskToEdit: 0,
-    editedTaskName: "",
-    editedTaskDate: ""
   }
 
   // This function gets the id of the box clicked, parses it into an integer, passes the integer into the completeTask method from the apiManager, and then loads all the tasks
@@ -19,55 +17,19 @@ export default class TaskCard extends Component {
       .then(this.props.loadTasks);
   }
 
-  // This function updates state to load the edit form and get the id of the task being edited
-  editTask = (evt) => {
-    evt.preventDefault();
-    const editId = parseInt(evt.target.id);
-    this.setState(
-      {
-        editingTask: true,
-        taskToEdit: editId,
-        editedTaskName: this.props.currentTask.description,
-        editedTaskDate: this.props.currentTask.date
-      }
-    );
-  }
-
-  // This function keeps track of all the changes to the input fields and stores them in state
-  handleFieldChange = (evt) => {
-    const stateToChange = {}
-    stateToChange[evt.target.id] = evt.target.value
-    this.setState(stateToChange)
-  }
-
-  // This function takes the new task description, the edited date and the task's id out of state, passes them to the apiManager, and then reloads all the tasks
-  saveTask = (evt) => {
-    evt.preventDefault();
-    const currentUser = sessionStorage.getItem("activeUser");
-    const taskId = this.state.taskToEdit;
-    const newDescription = this.state.editedTaskName;
-    const newDate = this.state.editedTaskDate;
-    apiManager.editTask(currentUser, taskId, newDescription, newDate)
-      .then(this.props.loadTasks, this.setState({ editingTask: false }))
+  hideEditTaskForm = () => {
+    this.setState({ editingTask: false })
   }
 
   render() {
     if (this.state.editingTask) {
       return (
-        <form onSubmit={this.saveTask} id={`${this.props.currentTask.id}form`}>
-          <label>Edit task:</label>
-          <input required type="text" id="editedTaskName"
-            onChange={this.handleFieldChange}
-            defaultValue={this.props.currentTask.description} />
-          <input required type="date" id="editedTaskDate"
-            min={this.props.today}
-            onChange={this.handleFieldChange}
-            defaultValue={this.props.currentTask.date} />
-          <button type="submit">Save changes</button>
-          <button onClick={() => {
-            this.setState({ editingTask: false })
-          }}>Cancel</button>
-        </form>
+        <TaskForm
+          currentTask={this.props.currentTask}
+          today={this.props.today}
+          hideForm={this.hideEditTaskForm}
+          loadTasks={this.props.loadTasks}
+        />
       )
     } else {
       return (
@@ -75,14 +37,9 @@ export default class TaskCard extends Component {
 
           <h3
             id={`${this.props.currentTask.id}edit`}
-            onClick={this.editTask}>
+            onClick={() => { this.setState({ editingTask: true }) }}>
             {this.props.currentTask.description} (click to edit)
-            </h3>
-
-          {/* Clicking the header allows you to edit, so I commented out this code */}
-          {/* <a href=""
-            id={`${this.props.currentTask.id}edit`}
-            onClick={this.editTask}>(edit)</a> */}
+          </h3>
 
           <p>Due: <Moment format="ddd, MMM Do, YYYY">
             {this.props.currentTask.date}
