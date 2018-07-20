fa2a27f9f610301b36fc950af3eb0462be573666
sdana <seth.dana@gmail.com>
2018-07-20 09:44:59 -0500

    fixed scrolling issues with header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,7 +5,7 @@ export default class Header extends Component {
     return (
     <React.Fragment>
     {/* <h1 className="header">Welcome to Nutshell!</h1 > */}
-    <div className="header">
+    <div className="header" id="main-image-container">
     <img className="headline-image" src="https://downloads.intercomcdn.com/i/o/2702/25ce9574b3a1dc309da496fc/Nutshell-logo-white%402x.png" />
     </div>
     </React.Fragment>
