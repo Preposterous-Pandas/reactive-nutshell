11f662ebc694cbba7c149497fcd1a63f822dc21c
Evan <ewlusky@gmail.com>
2018-07-23 15:43:05 -0500

    reddit news done

--- src/Components/News/News-Article.js
+++ src/Components/News/News-Article.js
@@ -9,9 +9,10 @@ export default props => {
                 <h5 className="card-title">
                     {props.article.title}
                 </h5>
-                <p className="card-body">{props.article.synopsis}</p>
-                <p className="card-body">{props.article.url}</p>
-                <p className="card-body"><Moment>{props.article.timestamp}</Moment></p>
+                {((/^(f|ht)tps?:\/\//i.test(props.article.synopsis))) ? <img src={`${props.article.synopsis}`} alt="an image" height="100" width="100" /> : <p className="card-content">{props.article.synopsis}</p>
+                }
+                <p className="card-content">{props.article.url}</p>
+                <p className="card-content"><Moment>{props.article.timestamp}</Moment></p>
 
                 <a href="#" onClick={() => props.checkOutarticle(props.article.id)}>Delete</a>
             </div>
